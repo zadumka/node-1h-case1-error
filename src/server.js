409b70860b867ae2c3cbdc165440cd5c54cc5b1a
zadumka <alenahas1987@gmail.com>
2025-07-28 14:01:42 +0300

    add branch

--- src/server.js
+++ src/server.js
@@ -1,26 +1,48 @@
 import express from 'express';
 import 'dotenv/config';
-import cors from 'cors';
+
+import pino from 'pino-http';
 
 const app = express();
+const PORT = process.env.PORT ?? 3030;
 
-const PORT = process.env.PORT || 8080; 
+// Middleware
+app.use(express.json());
 
-app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
-app.get('/note', (req, res) => {
+// Routes
+app.get('/notes', (req, res) => {
   res.status(200).json({
     message: 'Retrieved all notes',
   });
 });
 
-app.get('/note/:id', (req, res) => { 
-  const { id } = req.params; // 
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
   res.status(200).json({
-    message: `Retrieved note with ID: ${id}`,
+    message: `Retrieved note with ID: ${noteId}`,
   });
 });
 
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
 
 // Custom middleware
 app.use((req, res) => {
