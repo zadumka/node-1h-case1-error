ec1ecce035b0a5f784c7e14414619aa8cd065d2b
zadumka <90329284+zadumka@users.noreply.github.com>
2025-07-28 09:19:29 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,17 @@ import pino from 'pino-http';
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
+
+app.use((req, res, next) => {
+  if (req.path === '/notes' || req.path.startsWith('/notes/') || req.path === '/test-error') {
+    next();
+  } else {
+    res.status(404).json({
+      message: 'Route not found',
+    });
+  }
+});
+
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -45,6 +56,11 @@ app.get('/test-error', () => {
 });
 
 
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
