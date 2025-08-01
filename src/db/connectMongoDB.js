cca9578524b5838861fa579a3c1cbcc16d543ef7
zadumka <90329284+zadumka@users.noreply.github.com>
2025-08-01 15:36:18 +0300

    Update connectMongoDB.js

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,15 +1,10 @@
 import mongoose from 'mongoose';
 
-
-export const connectMongoDB = () => {
+export const connectMongoDB = async () => {
   const mongoUrl = process.env.MONGO_URL;
   
-  mongoose.connect(mongoUrl, (error) => {
-    if (error) {
-      console.error('❌ Failed to connect to MongoDB:', error.message);
-      process.exit(1);
-    } else {
-      console.log('✅ MongoDB connection established successfully');
-    }
-  });
+  await mongoose.connect(mongoUrl);
+  console.log('✅ MongoDB connection established successfully');
+  
 };
+
