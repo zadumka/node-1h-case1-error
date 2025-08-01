04445283fca6c79d58abfb1060090b039e7101a9
zadumka <90329284+zadumka@users.noreply.github.com>
2025-08-01 15:49:54 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import 'dotenv/config';
+
 import cors from 'cors';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -8,19 +8,18 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT || 3030; 
 
 app.use(express.json());
 app.use(cors());
+app.use(logger);
 
 app.use(notesRoutes);
-app.use(logger); 
 
+app.use(notFoundHandler);
 app.use(errorHandler);
-app.use(notFoundHandler); 
 
-
+await connectMongoDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-await connectMongoDB();
