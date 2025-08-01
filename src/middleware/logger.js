8bec867e22c18d9c95f1df0b3573264c22be711b
zadumka <90329284+zadumka@users.noreply.github.com>
2025-08-01 15:38:55 +0300

    Update logger.js

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,18 +1,8 @@
+import pino from 'pino-http';
 
-import { pino } from 'pino-http';
-
-
-export default pino({
-  level: 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname',
-      messageFormat:
-        '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-      hideObject: true,
-    },
-  },
+export const logger = pino({
+  
+  level: 'debug',
+ 
 });
+
