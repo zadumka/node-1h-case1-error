e5b5555157bd9ba4136d4bd949e42c4eaaa742a6
zadumka <90329284+zadumka@users.noreply.github.com>
2025-08-07 15:06:57 +0300

    Update notesRoutes.js

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,18 +1,19 @@
-import Router from 'express';
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
 import {
-  createNote,
-  deleteNote,
-  getAllNotes,
-  getNoteById,
-  updateNote,
-} from '../controllers/notesController.js';
+  createNoteSchema,
+  getAllNotesSchema,
+  noteIdSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
+
 
 const router = Router();
 
-router.get('/notes', getAllNotes);
-router.get('/notes/:noteId', getNoteById);
-router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNote);
-router.patch('/notes/:noteId', updateNote);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
+router.post('/notes', celebrate(createNoteSchema), createNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
