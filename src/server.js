4946ac68bd267451e09cbaafcf8f91f4a279816d
zadumka <90329284+zadumka@users.noreply.github.com>
2025-08-07 16:52:01 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-import express from 'express';
+"import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -11,14 +12,16 @@ const app = express();
 const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
-
+app.use(cors());
 app.use(logger);
 app.use(notesRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
 
-await connectMongoDB();
+// ❌ Підключення до MongoDB не дочікується перед запуском сервера
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+await connectMongoDB();"
