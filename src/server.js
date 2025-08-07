c4d6de8cd492dffa386e294b0a12552ca178f6fa
zadumka <90329284+zadumka@users.noreply.github.com>
2025-08-07 16:38:34 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
-import cors from 'cors';
 import { errors } from 'celebrate';
-import { logger } from './middleware/logger'; 
+import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
@@ -12,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
-app.use(cors());
+
 app.use(logger);
 app.use(notesRoutes);
 app.use(notFoundHandler);
