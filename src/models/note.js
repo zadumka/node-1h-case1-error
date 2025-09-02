482edc664041048760c06ce229496e7d3002a7d2
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-02 13:47:01 +0300

    Update note.js

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { model, Schema } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -18,17 +19,15 @@ const noteSchema = new Schema(
       required: false,
       default: 'Todo',
       
-      enum: [
-        'work',
-        'personal',
-        'meeting',
-        'shopping',
-        'ideas',
-        'todo',
-      ],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
   },
   {
+    timestamps: true,
     versionKey: false,
   },
 );
