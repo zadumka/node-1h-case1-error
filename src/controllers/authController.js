e0434656cd63087206b06744b04d2f80ee7e06d7
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-02 13:50:05 +0300

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,14 +12,12 @@ export const registerUser = async (req, res, next) => {
     return next(createHttpError(400, 'Email in use'));
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-
+ 
   const newUser = await User.create({
     email,
-    password: hashedPassword,
+    password,
   });
 
-  // Create new session and set cookies
   const newSession = await createSession(newUser._id);
   setSessionCookies(res, newSession);
 
@@ -39,59 +37,5 @@ export const loginUser = async (req, res, next) => {
     return next(createHttpError(401, 'Invalid credentials'));
   }
 
-  // Delete old session
   await Session.deleteOne({ userId: user._id });
-
-  // Create new session and set cookies
-  const newSession = await createSession(user._id);
-  setSessionCookies(res, newSession);
-
-  res.status(200).json(user);
-};
-
-export const logoutUser = async (req, res) => {
-  const { sessionId } = req.cookies;
-
-  if (sessionId) {
-    await Session.deleteOne({ _id: sessionId });
-  }
-
-  res.clearCookie('sessionId');
-  res.clearCookie('accessToken');
-  res.clearCookie('refreshToken');
-
-  res.status(204).send();
-};
-
-export const refreshUserSession = async (req, res, next) => {
-  // Find and check existing session
-  const session = await Session.findOne({
-    _id: req.cookies.sessionId,
-    refreshToken: req.cookies.refreshToken,
-  });
-
-  if (!session) {
-    return next(createHttpError(401, 'Session not found'));
-  }
-
-  const isSessionTokenExpired =
-    new Date() > new Date(session.refreshTokenValidUntil);
-
-  if (isSessionTokenExpired) {
-    return next(createHttpError(401, 'Session token expired'));
-  }
-
-  // Delete old session
-  await Session.deleteOne({
-    _id: req.cookies.sessionId,
-    refreshToken: req.cookies.refreshToken,
-  });
-
-  // Create new session and set cookies
-  const newSession = await createSession(session.userId);
-  setSessionCookies(res, newSession);
-
-  res.status(200).json({
-    message: 'Session refreshed',
-  });
-};
+  const newSession = a
