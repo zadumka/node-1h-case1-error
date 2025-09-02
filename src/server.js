955100cf26ee3e3d56ccbba75f64e0032c43a52f
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-02 14:13:59 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
@@ -21,11 +21,11 @@ app.use(notesRoutes);
 app.use(authRoutes);
 
 app.use(notFoundHandler);
-app.use(errors());
+
+
 app.use(errorHandler);
 
-
-app.listen(PORT, async () => {
-  await connectMongoDB();
+await connectMongoDB();
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
