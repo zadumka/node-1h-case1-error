92a63934c33d77f9cab02009aceb41420d7a67e6
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-02 13:49:02 +0300

    Update time.js

--- src/constants/time.js
+++ src/constants/time.js
@@ -1,2 +1,2 @@
-export const FIFTEEN_MINUTES = 15 * 60 * 1000;
-export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const FIFTEEN_MINUTES = 24 * 60 * 60 * 1000; 
+export const ONE_DAY = 15 * 60 * 1000; 
