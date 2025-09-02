82ad9f8d911192c66a88091554353bdf1d08d17b
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-02 14:19:53 +0300

    Update notesController.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -3,24 +3,23 @@ import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search = '' } = req.query;
-  const skip = (page - 1) * perPage;
 
-  const notesQuery = Note.find({});
+  const notesQuery = Note.find({ userId: req.user._id });
 
   if (tag) {
-    notesQuery.where('tag').equals(tag);
+    notesQuery.where('tag').regex(tag); 
   }
 
   if (search) {
     notesQuery.or([
-      { title: { $regex: search, $options: 'i' } },
-      { content: { $regex: search, $options: 'i' } },
+      { title: { $regex: search } }, 
+      { content: { $regex: search } },
     ]);
   }
 
   const [totalNotes, notes] = await Promise.all([
     notesQuery.clone().countDocuments(),
-    notesQuery.skip(skip).limit(perPage),
+    notesQuery.limit(perPage), 
   ]);
   const totalPages = Math.ceil(totalNotes / perPage);
 
@@ -34,33 +33,7 @@ export const getAllNotes = async (req, res) => {
 };
 
 export const getNoteById = async (req, res, next) => {
-  const note = await Note.findById(req.params.noteId); 
-
-  if (!note) {
-    return next(createHttpError(404, 'Note not found'));
-  }
-
-  res.status(200).json(note);
-};
-
-export const createNote = async (req, res) => {
-  const newNote = await Note.create(req.body); 
-  res.status(200).json(newNote); 
-};
-
-export const deleteNote = async (req, res, next) => {
-  const note = await Note.findByIdAndDelete(req.params.noteId); 
-  if (!note) {
-    return next(createHttpError(404, 'Note not found'));
-  }
-
-  res.status(200).json(note);
-};
-
-export const updateNote = async (req, res, next) => {
-  const note = await Note.findByIdAndUpdate(req.params.noteId, req.body, {
-    new: true,
-  }); 
+  const note = await Note.findOne({ _id: req.params.noteId }); 
 
   if (!note) {
     return next(createHttpError(404, 'Note not found'));
