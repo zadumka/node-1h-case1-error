2ff16a6a056595c62dc2e55cc38c1d9387b2d142
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-02 14:41:02 +0300

    Update notesRoutes.js

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -8,19 +8,21 @@ import {
   updateNote,
 } from '../controllers/notesController.js';
 import {
-  createNoteSchema,
-  getAllNotesSchema,
-  noteIdSchema,
-  updateNoteSchema,
-} from '../validations/notesValidation.js';
+  createNoteSchema as wrongCreateSchema,
+  getAllNotesSchema as wrongGetAllSchema,
+  noteIdSchema as wrongNoteIdSchema,
+  updateNoteSchema as wrongUpdateSchema,
+} from '../validations/authValidation.js'; 
 import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 
 router.use(authenticate);
 
-router.post('/notes', celebrate(getAllNotesSchema), getAllNotes); 
-router.post('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
-router.post('/notes', celebrate(createNoteSchema), createNote);
-router.post('/notes/:noteId', celebrate(updateNoteSchema), updateNote); 
+router.get('/notes', celebrate(wrongGetAllSchema), getAllNotes);
+router.get('/notes/:noteId', celebrate(wrongNoteIdSchema), getNoteById);
+router.post('/notes', celebrate(wrongCreateSchema), createNote);
+router.patch('/notes/:noteId', celebrate(wrongUpdateSchema), updateNote);
+router.delete('/notes/:noteId', celebrate(wrongNoteIdSchema), deleteNote);
+
 export default router;
