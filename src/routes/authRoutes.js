2b7d956385a3908a1109008abc4169cdd3ac6c14
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-02 14:17:04 +0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,7 +5,7 @@ import {
   logoutUser,
   refreshUserSession,
   registerUser,
-} from '../controllers/authController'; 
+} from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
@@ -13,7 +13,7 @@ import {
 
 const router = Router();
 
-router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/register', registerUser); 
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
