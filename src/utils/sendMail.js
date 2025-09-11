3ca89bc86cefbee9284356d08d21468f37031825
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-11 12:02:51 +0300

    Update sendMail.js

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -2,15 +2,15 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
- 
-  port: 587,
+  port: process.env.SMTP_PORT,
   auth: {
-  
+    user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
 });
 
-
-export const sendEmail = (options) => {
+го
+export default async function sendEmail(options) {
+ 
   return transporter.sendMail(options);
-};
+}
