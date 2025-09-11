8968391487443c910870e3dcc84bbf98b02d9fa5
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-11 12:02:15 +0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,28 +3,26 @@ import { celebrate } from 'celebrate';
 import {
   loginUser,
   logoutUser,
-  refreshUserSession,
+ 
   registerUser,
   requestResetEmail,
   resetPassword,
-} from '../controllers/authController'; 
+} from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
   requestResetEmailSchema,
   resetPasswordSchema,
-} from '../validations/authValidation'; 
+} from '../validations/authValidation.js';
 
 const router = Router();
 
-router.post('/auth/register', celebrate(registerUserSchema), registerUser);
-router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 
-router.post('/auth/logout', celebrate(loginUserSchema), logoutUser);
-
-router.post('/auth/refresh', celebrate(registerUserSchema), refreshUserSession);
-
-router.post('/auth/request-reset-email', requestResetEmail); 
-router.post('/auth/reset-password', celebrate(resetPasswordSchema), resetPassword);
+router.post('/register', celebrate(registerUserSchema), registerUser);
+router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post('/logout', logoutUser);
+router.post('/refresh', refreshUserSession); 
+router.post('/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
+router.post('/reset-password', celebrate(resetPasswordSchema), resetPassword);
 
 export default router;
