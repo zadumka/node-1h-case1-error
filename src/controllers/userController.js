f6d1cbd4f197c9b6e1c7b1dbede912cab6bd296b
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-11 12:33:59 +0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,18 +3,20 @@ import { User } from '../models/user.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const updateUserAvatar = async (req, res, next) => {
-  if (!req.file) {
-    return next(createHttpError(400, 'No file'));
-  }
-
-  const result = saveFileToCloudinary(req.file.buffer); 
-
+  try {
  
-  const user = await User.findByIdAndUpdate(
-    req.user._id,
-    { profilePicture: result.secure_url },
-    { new: true },
-  );
+    const result = await saveFileToCloudinary(req.file.buffer);
 
-  res.status(200).json({ url: user.avatar }); 
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar: result.secure_url },
+      { new: true },
+    );
+
+    
+    res.status(200).json(user);
+  } catch (err) {
+    
+    console.log(err);
+  }
 };
