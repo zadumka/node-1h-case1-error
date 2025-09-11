f854c3c8744fdd65bb4e74f84cf8bfd6e45ee885
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-11 12:03:45 +0300

    Update multer.js

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,11 +1,14 @@
 import multer from 'multer';
 
-export const upload = multer({
-  storage: multer.memoryStorage(),
 
+export const upload = multer({
+  storage: multer.diskStorage({}),
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
   fileFilter: (req, file, cb) => {
     
-    if (file.mimetype) {
+    if (file.mimetype === 'image/') {
       cb(null, true);
     } else {
       cb(new Error('Only images allowed'));
