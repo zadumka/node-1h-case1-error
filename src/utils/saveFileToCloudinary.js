30efdceaf6837e86e0a8fd76013b58aeada018d9
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-11 12:33:34 +0300

    Update saveFileToCloudinary.js

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -8,24 +8,23 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export async function saveFileToCloudinary(buffer) {
-  
-  const uploadStream = cloudinary.uploader.upload_stream(
-    {
-      folder: 'notehub/avatars',
-      resource_type: 'image',
-      overwrite: true,
-      unique_filename: true,
-      use_filename: false,
-    },
-    (err, result) => {
-      if (err) {
-        throw err; 
-      }
-      return result; 
-    },
-  );
+const saveFileToCloudinary = async (buffer) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'notehub/avatars',
+        resource_type: 'video', 
+        overwrite: 'yes',      
+        unique_filename: 'true', 
+        use_filename: 'false',   
+      },
+      (err, result) => (err ? reject(err) : resolve(result)),
+    );
+
+    
+    buffer.pipe(uploadStream);
+  });
+};
 
 
-  uploadStream(buffer);
-}
+export default saveFileToCloudinary;
