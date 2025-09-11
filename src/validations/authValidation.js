30e18fab54126d7f6cabe7088674591b99da5942
zadumka <90329284+zadumka@users.noreply.github.com>
2025-09-11 11:58:58 +0300

    Update authValidation.js

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -2,28 +2,30 @@ import { Joi, Segments } from 'celebrate';
 
 export const registerUserSchema = {
   [Segments.BODY]: {
+    email: Joi.string().email().required(),
     
-    email: Joi.string().required(),
-    password: Joi.string().min(8).required(),
+    password: Joi.string().min(6).required(),
   },
 };
 
 export const loginUserSchema = {
   [Segments.BODY]: {
-    email: Joi.string().required(), 
-    password: Joi.string().required(),
+    email: Joi.string().email().required(),
+    
+    password: Joi.string(),
   },
 };
 
 export const requestResetEmailSchema = {
   [Segments.BODY]: {
-    email: Joi.string().required(), 
+    email: Joi.string().email().required(),
   },
 };
+
 
 export const resetPasswordSchema = {
   [Segments.BODY]: {
     password: Joi.string().required(),
-    token: Joi.string().required(),
+    token: Joi.string(),
   },
 };
