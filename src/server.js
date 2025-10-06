5d79fb1b3b73315f93edafa9ccf7dce4acb61059
zadumka <90329284+zadumka@users.noreply.github.com>
2025-10-06 15:00:22 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,63 +1,50 @@
-import express from 'express';
-import cors from 'cors';
-import pino from 'pino-http';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pinoHttp from "pino-http";
 
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3030; 
 const app = express();
 
 
-const PORT = process.env.PORT ?? 3030;
-
-// Middleware
+app.use(cors()); 
 app.use(express.json());
-app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(pinoHttp()); 
 
-// Routes
-app.get('/notes', (req, res) => {
+
+app.get("/notes", (req, res) => {
   res.status(200).json({
-    message: 'Retrieved all notes',
+    message: "Retrieved all notes",
   });
 });
 
-app.get('/notes/:noteId', (req, res) => {
+app.get("/notes/:noteId", (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({
     message: `Retrieved note with ID: ${noteId}`,
   });
 });
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
+app.get("/test-error", () => {
+  throw new Error("Simulated server error");
 });
 
-// Custom middleware
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Route not found',
+    message: "Route not found",
   });
 });
 
 app.use((err, req, res, next) => {
+  console.error(err.message);
   res.status(500).json({
-    message: err.message,
+    message: err.message || "Internal server error",
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
