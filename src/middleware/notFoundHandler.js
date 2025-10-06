f604770a4b7fa8ae286b746dee8f5ba7333a6643
zadumka <90329284+zadumka@users.noreply.github.com>
2025-10-06 16:20:01 +0300

    Update notFoundHandler.js

--- src/middleware/notFoundHandler.js
+++ src/middleware/notFoundHandler.js
@@ -1,7 +1,3 @@
-
-export const notFound = () => {
-  
-  res.status(404).json({
-    message: 'Route not found',
-  });
+export const notFoundHandler = (req, res) => {
+res.status(404).json({ "message": "Route not found" });
 };
