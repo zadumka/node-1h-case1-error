f707a2d0c7ed4bb0d12a5f015ac1028ceb3db588
zadumka <90329284+zadumka@users.noreply.github.com>
2025-10-06 16:21:33 +0300

    Update notesRoutes.js

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,13 +1,8 @@
-import Router from 'express';
-import {
-  createNote,
-  deleteNote,
-  getAllNotes,
-  getNoteById,
-  updateNote,
-} from '../controllers/notesController.js';
+import { Router } from "express";
+import { getAllNotes, getNoteById, createNote, deleteNote, updateNote } from "../controllers/notesController.js";
 
 const router = Router();
+
 
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
