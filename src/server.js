c22cf6f40db95027edd42cbb9b6ffac41edc52d3
zadumka <90329284+zadumka@users.noreply.github.com>
2025-10-06 15:07:59 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,31 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import pinoHttp from "pino-http";
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3030; 
 const app = express();
+const PORT = process.env.PORT ?? 3030;
 
-
-app.use(cors()); 
 app.use(express.json());
-app.use(pinoHttp()); 
-
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello world!' });
+});
 
 app.get("/notes", (req, res) => {
   res.status(200).json({
@@ -23,28 +35,29 @@ app.get("/notes", (req, res) => {
 
 app.get("/notes/:noteId", (req, res) => {
   const { noteId } = req.params;
+
   res.status(200).json({
     message: `Retrieved note with ID: ${noteId}`,
   });
 });
 
-app.get("/test-error", () => {
-  throw new Error("Simulated server error");
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
 });
 
 app.use((req, res) => {
-  res.status(404).json({
-    message: "Route not found",
-  });
+  res.status(404).json({ message: 'Route not found' });
 });
 
 app.use((err, req, res, next) => {
-  console.error(err.message);
+  console.error('Error:', err.message);
   res.status(500).json({
-    message: err.message || "Internal server error",
+    message: 'Simulated server error',
+    error: err.message,
   });
 });
 
+// Запуск сервера
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
