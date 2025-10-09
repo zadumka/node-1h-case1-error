e96ff3c5872dffbb557d28dc935e1502bcaf0fd1
zadumka <90329284+zadumka@users.noreply.github.com>
2025-10-09 14:01:07 +0300

    Update note.js

--- src/models/note.js
+++ src/models/note.js
@@ -1,22 +1,35 @@
 import { model, Schema } from 'mongoose';
 import { TAGS } from '../constants/tags.js';
 
-const noteSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      default: '',
+      trim: true,
+    },
+    tag: {
+      type: String,
+      required: false,
+      default: 'Todo',
+      enum: [...TAGS],
+    },
   },
-  content: {
-    type: String,
-    default: '',
-    trim: true,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  tag: {
-    type: String,
-    default: 'Todo',
-    enum: [...TAGS],
-  },
-}); 
+);
+
+noteSchema.index({
+  title: 'text',
+  content: 'text',
+});
 
 export const Note = model('Note', noteSchema);
