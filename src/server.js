2a4f91763411b50c48d858d28b0f759329b3ded5
zadumka <90329284+zadumka@users.noreply.github.com>
2025-11-13 16:51:51 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import 'dotenv/config';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
@@ -14,22 +13,22 @@ import userRoutes from './routes/userRoutes.js';
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
+
+app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(logger);
 
 app.use(notesRoutes);
 app.use(authRoutes);
 app.use(userRoutes);
 
+app.use(errors()); 
 app.use(notFoundHandler);
-app.use(errors());
 app.use(errorHandler);
 
+await connectMongoDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+  console.log(`Server started at ${PORT}`); 
 });
-
-await connectMongoDB(); 
