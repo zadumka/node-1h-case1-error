4a40233db9495570a8abc0f4892824e5ab9fc23b
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-16 15:50:49 +0200

    Refactor server.js for improved routing and config
    Refactor server.js to include cookie-parser and auth routes, and adjust dotenv configuration.

--- src/server.js
+++ src/server.js
@@ -1,25 +1,39 @@
 import express from 'express';
-import cors from "cors";
-import dotenv from 'dotenv';
-import { connectMongoDB } from "./db/connectMongoDB.js";
-import notesRouter from './routes/notesRoutes.js';
+import cors from 'cors';
+import 'dotenv/config';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import { errors } from 'celebrate';
+
 import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
+import cookieParser from "cookie-parser";
 
-dotenv.config();
+import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
+
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 
-app.use(notesRouter);
+app.use(authRoutes);
+app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
+
+app.use(errors());
+
 app.use(errorHandler);
 
 await connectMongoDB();
@@ -27,3 +41,5 @@ await connectMongoDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
