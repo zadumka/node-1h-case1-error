387213490fd70d12fffc8fb6f8b0f755759dab4a
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-16 16:15:24 +0200

    Refactor authController to improve response structure
    Updated response format to include user data in JSON format for register and login functions. Fixed cookie access in refreshUserSession function.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,17 +1,21 @@
-import createHttpError from 'http-errors';
-import { User } from '../models/user.js';
-import bcrypt from 'bcrypt';
-import { createSession, setSessionCookies } from '../services/auth.js';
-import { Session } from '../models/session.js';
+import bcrypt from "bcrypt";
+import createHttpError from "http-errors";
+import { User } from "../models/user.js";
 
+import { createSession, setSessionCookies } from "../services/auth.js";
+import { Session } from "../models/session.js";
+
+/*registerUser*/
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
+
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    return next(createHttpError(400, 'Email in use'));
+    return next(createHttpError(400, "Email in use"));
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
+
   const newUser = await User.create({
     email,
     password: hashedPassword,
@@ -20,35 +24,42 @@ export const registerUser = async (req, res, next) => {
   const newSession = await createSession(newUser._id);
   setSessionCookies(res, newSession);
 
-  res.status(201).json(newUser);
+  res.status(201).json({
+user: newUser.toJSON(),
+  });
 };
 
+/*loginUser*/
 export const loginUser = async (req, res, next) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
   if (!user) {
     return next(createHttpError(401, 'User not found'));
   }
-
   const isValidPassword = await bcrypt.compare(password, user.password);
   if (!isValidPassword) {
     return next(createHttpError(401, 'Invalid credentials'));
   }
 
   await Session.deleteOne({ userId: user._id });
-
   const newSession = await createSession(user._id);
-
   setSessionCookies(res, newSession);
 
-  res.status(200).json(user);
+
+  res.status(200).json({
+  user: user.toJSON(),
+  });
 };
 
+/*logoutUser*/
 export const logoutUser = async (req, res) => {
   const { sessionId } = req.cookies;
+
   if (sessionId) {
     await Session.deleteOne({ _id: sessionId });
   }
+
   res.clearCookie('sessionId');
   res.clearCookie('accessToken');
   res.clearCookie('refreshToken');
@@ -56,24 +67,29 @@ export const logoutUser = async (req, res) => {
   res.status(204).send();
 };
 
+
+
+/*refreshUserSession*/
 export const refreshUserSession = async (req, res, next) => {
   const session = await Session.findOne({
-    _id: req.cookie.sessionId,
-    refreshToken: req.cookie.refreshToken,
+    _id: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
   });
-
-  if (!session) {
+if (!session) {
     return next(createHttpError(401, 'Session not found'));
   }
-  const isSessionTokenExpired =
+
+  const isRefreshTokenExpired =
     new Date() > new Date(session.refreshTokenValidUntil);
-  if (isSessionTokenExpired) {
+  if (isRefreshTokenExpired) {
     return next(createHttpError(401, 'Session token expired'));
   }
+
   await Session.deleteOne({
     _id: req.cookies.sessionId,
     refreshToken: req.cookies.refreshToken,
   });
+
   const newSession = await createSession(session.userId);
   setSessionCookies(res, newSession);
 
