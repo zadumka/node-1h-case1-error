81f893edf0571c844b3eff4ae31e14f3b209e2f1
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-16 16:23:31 +0200

    Move logger middleware to the correct position

--- src/server.js
+++ src/server.js
@@ -13,10 +13,10 @@ import authRoutes from './routes/authRoutes.js';
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
+app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(logger);
 
 app.use(authRoutes);
 app.use(notesRoutes);
