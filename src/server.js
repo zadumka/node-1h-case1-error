db7d278795384dc7a05becc36d49e303c79d80e4
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-16 15:59:17 +0200

    Refactor server.js for improved structure and clarity

--- src/server.js
+++ src/server.js
@@ -1,31 +1,34 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import { errors } from "celebrate";
+import { connectMongoDB } from "./db/connectMongoDB.js";
+import { logger } from "./middleware/logger.js";
+import { notFoundHandler } from "./middleware/notFoundHandler.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import notesRoutes from "./routes/notesRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
-import { logger } from './middleware/logger'; 
-import { notFoundHandler } from './middleware/notFoundHandler'; 
-import { errorHandler } from './middleware/errorHandler'; 
-import { connectMongoDB } from './db/connectMongoDB'; 
-import notesRoutes from './routes/notesRoutes'; 
-import authRoutes from './routes/authRoutes'; 
-
 const app = express();
-const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(logger);
-
-app.use(notesRoutes);
 app.use(authRoutes);
-
-app.use(notFoundHandler);
+app.use(notesRoutes);
 app.use(errors());
+app.use(notFoundHandler);
 app.use(errorHandler);
 
-await connectMongoDB();
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3030;
+
+const startServer = async () => {
+  await connectMongoDB();
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
