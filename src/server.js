fc91c94286c90d2d2d717ad4389ac2d99db5df1b
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-16 16:26:27 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import 'dotenv/config';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
+import pino from 'pino-http';
+import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
-import authRoutes from './routes/authRoutes.js';
+import { errors } from 'celebrate';
+import authRoutes from "./routes/authRoutes.js";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -17,18 +18,30 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 app.use(authRoutes);
 app.use(notesRoutes);
-
 app.use(notFoundHandler);
-
 app.use(errors());
-
 app.use(errorHandler);
 
 await connectMongoDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
