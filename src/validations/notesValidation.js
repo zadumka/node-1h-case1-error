46df533609d2e898bf9f6160afbc41b989dd0482
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-16 13:43:24 +0200

    Enhance note validation schemas with sorting and ID checks
    Refactor validation schemas to include sorting options and improve object ID validation messages.

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,10 +1,6 @@
 import { Joi, Segments } from 'celebrate';
-
+import { TAGS } from '../constants/tags.js';
 import { isValidObjectId } from 'mongoose';
-
-const objectIdValidator = (value, helpers) => {
-  return isValidObjectId(value) ? value : helpers.message('Invalid id');
-};
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
@@ -12,28 +8,36 @@ export const getAllNotesSchema = {
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().trim().allow(''),
+    sortBy: Joi.string().valid('_id', 'tag').default('_id'),
+    sortOrder: Joi.string().valid('asc', 'desc').default('asc'),
   }),
+};
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator)
+  })
 };
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
-    content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS).required(),
-  }),
-};
-
-export const noteIdSchema = {
-  [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().custom(objectIdValidator).required(),
-  }),
+    tag: Joi.string().valid(...TAGS),
+    content: Joi.string().trim().allow(''),
+  })
 };
 
 export const updateNoteSchema = {
-  ...noteIdSchema,
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator),
+  }),
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1),
-    content: Joi.string().allow(''),
+    content: Joi.string().trim().allow(''),
     tag: Joi.string().valid(...TAGS),
-  }), 
+  }).min(1),
 };
