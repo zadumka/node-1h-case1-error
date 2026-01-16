0897934869939241310c93c755cfaec55dd4bcf6
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-16 15:44:04 +0200

    Refactor server.js and update port number

--- src/server.js
+++ src/server.js
@@ -1,37 +1,25 @@
 import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
-
-import { connectMongoDB } from './db/connectMongoDB.js';
+import cors from "cors";
+import dotenv from 'dotenv';
+import { connectMongoDB } from "./db/connectMongoDB.js";
+import notesRouter from './routes/notesRoutes.js';
 import { logger } from './middleware/logger.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import notesRoutes from './routes/notesRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.use(helmet());
-app.use(cookieParser());
-app.use(cors());
+const PORT = process.env.PORT || 3030;
 
 app.use(logger);
-
-app.use(authRoutes);
-app.use(notesRoutes);
-app.use(userRoutes);
+app.use(express.json());
+app.use(cors());
 
 
+app.use(notesRouter);
 
 app.use(notFoundHandler);
-app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
