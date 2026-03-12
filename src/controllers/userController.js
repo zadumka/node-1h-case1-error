78d964b79ec50ef88e3f7d5176a9eef7f793f9d6
zadumka <90329284+zadumka@users.noreply.github.com>
2026-03-12 11:58:26 +0200

    Refactor updateUserAvatar to handle missing file

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,21 +2,18 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
-export const updateUserAvatar = async (req, res, next) => {
-  try {
- 
-    const result = await saveFileToCloudinary(req.file.buffer);
-
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      { avatar: result.secure_url },
-      { new: true },
-    );
-
-    
-    res.status(200).json(user);
-  } catch (err) {
-    
-    console.log(err);
+export const updateUserAvatar = async (req, res) => {
+  if (!req.file) {
+    throw createHttpError(400, 'No file');
   }
+
+  const result = await saveFileToCloudinary(req.file.buffer);
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { avatar: result.secure_url },
+    { returnDocument: 'after' },
+  );
+
+  res.status(200).json({ url: user.avatar });
 };
